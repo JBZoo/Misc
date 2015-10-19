3359e24374de4ca33e4aad298270ea80a4f9278b
SmetDenis <denis@jbzoo.com>
2015-10-19 20:07:49 +0600

    private/protected method/variables must be prefixed with an underscore + tests

---
+++ phpcs/JBZoo/Sniffs/TestClassUnderscoreFalse.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * JBZoo Misc
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   Misc
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ * @link      https://github.com/JBZoo/Misc
+ * @author    Denis Smetannikov <denis@jbzoo.com>
+ */
+
+namespace JBZoo\Misc;
+
+/**
+ * Class TestClassUnderscoreFalse
+ * @package JBZoo\Misc
+ */
+class TestClassUnderscoreFalse
+{
+    /**
+     * @var string
+     */
+    private $private = '';
+
+    /**
+     * @var string
+     */
+    protected $protected = '';
+
+    /**
+     * @var string
+     */
+    public $_public = '';
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    private function privateFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    protected function protectedFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function _publicFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function __myMagic()
+    {
+        return '';
+    }
+}
---
+++ phpcs/JBZoo/Sniffs/TestClassUnderscoreTrue.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * JBZoo Misc
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   Misc
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ * @link      https://github.com/JBZoo/Misc
+ * @author    Denis Smetannikov <denis@jbzoo.com>
+ */
+
+namespace JBZoo\Misc;
+
+/**
+ * Class TestClassUnderscoreTrue
+ * @package JBZoo\Misc
+ */
+class TestClassUnderscoreTrue
+{
+    /**
+     * @var string
+     */
+    private $_private = '';
+
+    /**
+     * @var string
+     */
+    protected $_protected = '';
+
+    /**
+     * @var string
+     */
+    public $public = '';
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    private function _privateFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    protected function _protectedFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function publicFunc($text = '')
+    {
+        return $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return '';
+    }
+}
